75ed4ebacff1027a1214eb3fe99f0314a215f17b
Frankie Ferreira <frankiemferreira@gmail.com>
2016-11-12 01:43:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime? (number)
+    if number < 2
+      return false
+    end
+    for n in 2...number
+      if number % n == 0
+        return false
+      end
+    end
+    return true
+  end
